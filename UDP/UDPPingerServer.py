896566c9eb2ef64318105d1b661b6a2fcc7f84d9
pratimakandel <pratimakandel40@gmail.com>
2020-07-12 12:27:12 -0400

    Update UDPPingerServer.py

--- UDP/UDPPingerServer.py
+++ UDP/UDPPingerServer.py
@@ -1,12 +1,10 @@
 # UDPPingerServer.py
-# We will need the following module to generate randomized lost packets
 import random
 import time
 from socket import *
 # Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-# Assign IP address and port number to socket
+
 serverSocket.bind(('', 12000))
 print("The server is Ready")
 while True:
@@ -14,7 +12,7 @@ while True:
  rand = random.randint(0, 10)
  # Receive the client packet along with the address it is coming from
  message, address = serverSocket.recvfrom(1024)
- # Capitalize the message from the client
+
  message = message.upper()
  msg = message.decode("utf-8").split(" ")
  time1 = time.localtime(float(msg[2])) 
